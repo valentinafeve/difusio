264d2ed10a1ae77add04347892d80e55dd869f6a
valntinaf <feruerevalentina@gmail.com>
2019-11-04 14:58:02 -0500

    Printing info

--- graph.py
+++ graph.py
@@ -12,10 +12,13 @@ import time
 import datetime
 import pandas as pd
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css','https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 dict = {'G':nx.random_geometric_graph(1, 1), 'rate': 0.2, 'infected_nodes_info': {}, 'clicks':0}
+wait_ies = {'drawing': True}
+results = {'s_results': []}
 
 def get_my_graph():
     return dict['G']
@@ -26,42 +29,135 @@ def set_my_graph(temp_graph):
 app.layout = html.Div(className='row', children=[
         html.Div([
         html.H1('Virus spread simulation', style={'color': '#333399', 'fontSize': 26}),
-        html.P('Nodos totales', className='my-class', id='n_p'),
         html.Div([
-            dcc.Slider(id="nodes_num", min=5, max=50, value=30, step=2, updatemode="drag",
-                         marks={5: "5", 10: "10", 20: "20", 30: "30", 40: "40", 50: "50"}, className="row"),
-            html.Div(id='nodes_num_container')
-        ]),
-        html.P('Nodos infectados', className='my-class', id='i_p'),
+            html.P('Nodos totales', className='my-class', id='n_p'),
+            html.Div([
+            dcc.Slider(id="nodes_num", min=5, max=100, value=10, step=10, updatemode="drag",
+            marks={5: "5", 10: "10", 20: "20", 30: "30", 40: "40", 50: "50", 60:"60", 70:"70", 80:"80", 90:"90", 100:"100"}, className="row"),
+            html.Div(id='nodes_num_div')
+            ], style={'margin-bottom': '20px', 'fontSize': 14}),
+        ], style={'margin':'20px'}),
         html.Div([
-            dcc.Slider(id="nodes_infected", min=5, max=50, value=10, step=1, updatemode="drag",
-                         marks={5: "5", 10: "10", 20: "20", 30: "30", 40: "40", 50: "50"}, className="row"),
-            html.Div(id='nodes_num_container_uno')
-        ]),
-        html.P('Radio', className='my-class', id='r_p', style={'margin-top':'20px'}),
+            html.P('Nodos infectados', className='my-class', id='i_p'),
+            html.Div([
+            dcc.Slider(id="nodes_infected", min=5, max=100, value=5, step=1, updatemode="drag",
+            marks={5: "5", 10: "10", 20: "20", 30: "30", 40: "40", 50: "50"}, className="row"),
+            html.Div(id='nodes_i_div'),
+            ], style={'margin-bottom': '20px', 'fontSize': 14}),
+        ],style={'margin':'20px'}),
         html.Div([
-            dcc.Slider(id="radius", min=0, max=1, value=0.2, step=0.1, className="row"),
-            html.P('Nodos infectados', className='my-class', id='i_radius'),
-            html.Div(id='nodes_num_container_dos')
-        ]),
+            html.P('Radio', className='my-class', id='r_p'),
+            html.Div([
+            dcc.Slider(id="radius", min=0, max=1, value=0.5, step=0.1,
+            className="row"),
+            html.Div(id='nodes_rad_div'),
+            ], style={'marginBottom': '20px', 'fontSize': 14}),
+        ],style={'margin':'20px'}),
         html.Button('Simulate', id='button', className='ui fluid green button'),
-        html.Div(id='button_output')
+        html.Div([
+            html.Div([
+                html.Div([
+                html.P('Results')
+                ], className='header'),
+                html.P('All results will be shown here.', id='p_results')
+            ], className='content')
+        ],className='ui card')
+
+
         ], className='three columns', style={'margin': '30px', 'fontSize': 14}),
         html.Div([
+        # Primer grafo
         dcc.Graph(id="my-graph"),
         dcc.Interval(
             id='interval-component',
             n_intervals=0,
-            interval=5*1000,
+
+            # Tiempo en segundos
+            interval=10000,
         )
         ], className='eight columns'),
     ])
 
+
+def update_infected():
+    G = dict['G']
+    rate = dict['rate']
+    print("Updating infected nodes...")
+    # Updating infected
+    infected_nodes_info = dict['infected_nodes_info'].copy()
+    temp_infected_nodes_info = infected_nodes_info.copy()
+    # For each node in infected ones
+    for node in infected_nodes_info:
+        # For each neighbor of an infected node
+        for neighbor in G.neighbors(node):
+            # If is not infected
+            if not neighbor in infected_nodes_info:
+                posibility = [True] * int(rate * 10) + [False] * int((1-rate)*10)
+                if random.choice(posibility):
+                    # Infect
+                    temp_infected_nodes_info[neighbor] = True
+    # Update infected info
+    dict['infected_nodes_info'] = temp_infected_nodes_info
+    print("Infected nodes updated")
+    return
+
+def draw_a_graph():
+    G = dict['G'].copy()
+    infected_nodes_info = dict['infected_nodes_info']
+    print("Drawing graph")
+    pos = nx.get_node_attributes(G, 'pos')
+    dmin = 1
+    ncenter = 0
+    for abigail in pos:
+        x, y = pos[abigail]
+        d = (x - 0.5) ** 2 + (y - 0.5) ** 2
+        if d < dmin:
+            ncenter = abigail
+            dmin = d
+    edge_trace = go.Scatter(x=[], y=[], line={'width': 0.5, 'color': '#888'}, hoverinfo='none', mode='lines')
+    nodes = G.nodes()
+    edges = G.edges()
+    for edge in edges:
+        x0, y0 = nodes[edge[0]]['pos']
+        x1, y1 = nodes[edge[1]]['pos']
+        edge_trace['x'] += tuple([x0, x1, None])
+        edge_trace['y'] += tuple([y0, y1, None])
+    node_trace = go.Scatter(x=[], y=[], text=[], mode='markers', hoverinfo='text',
+                            marker={'showscale': True, 'colorscale': 'Picnic', 'reversescale': True, 'color': [],
+                                    'size': 10,
+                                    'colorbar': {'thickness': 10, 'xanchor': 'left',
+                                                 'titleside': 'right'},
+                                    'line': {'width': 2}})
+    nodes = G.nodes()
+    to_iterate_arr = range(len(nodes))
+    for i in to_iterate_arr:
+        x, y = nodes[i]['pos']
+        node_trace['x'] += tuple([x])
+        node_trace['y'] += tuple([y])
+    p = nx.single_source_shortest_path_length(G, ncenter)
+    adjas = enumerate(G.adjacency())
+    for node, adjacencies in adjas:
+        color_node = 2
+        if node in infected_nodes_info:
+            color_node = 0
+        node_trace['marker']['color'] += tuple([color_node])
+        node_info = 'Is infected: ' + str(node in infected_nodes_info)
+        node_trace['text'] += tuple([node_info])
+    figure = {"data": [edge_trace, node_trace],
+              "layout": go.Layout(showlegend=False, hovermode='closest',
+                                  margin={'b': 20, 'l': 5, 'r': 5, 't': 40},
+                                  xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                                  yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False})}
+    print("Graph drawn")
+    return figure
+
 @app.callback(
-    Output("my-graph", "figure"),
     [
-    Input('button', 'n_clicks'),
-    Input('interval-component', 'n_intervals')
+    Output("my-graph", "figure"),
+    Output("p_results", "children")
+    ],
+    [Input('button', 'n_clicks'),
+    Input('interval-component', 'n_intervals'),
     ],
     [
     State("nodes_num", "value"),
@@ -71,128 +167,65 @@ app.layout = html.Div(className='row', children=[
     )
 
 
-def create_graph(n_clicks, interval, num_nodes, nodes_infected, radius):
-    print("Interval: %d" % interval)
-    print("n: %d" % num_nodes)
-    print("infected: %d" % nodes_infected)
+def m_graph(n_clicks, interval_n, nodes_num, nodes_infected, radius):
+    if n_clicks is None or n_clicks > dict['clicks']:
+        print("Clicked")
+        print("Creating graph")
+        dict['clicks']+=1
+        dict['infected_nodes_info']={}
+        infected_nodes_info = dict['infected_nodes_info']
+        infected = 0
+        cont = 0
+
+        # Randomly infecting nodes
+        while infected < nodes_infected and cont<nodes_num:
+            print("Infecting...")
+            randy = random.randint(0, nodes_num-1)
+            if randy not in infected_nodes_info:
+                infected_nodes_info[randy] = True
+                infected += 1
+            cont += 1
+
+        print("Creating graph with %d nodes and %d as radius. Infecting %d nodes." % (nodes_num, radius, nodes_infected))
+        G = nx.random_geometric_graph(nodes_num, radius)
+        dict['G'] = G
+        print("Trying to draw created")
+        figure = draw_a_graph()
+        wait_ies['drawing']=False
+        return figure, [html.P("Creating graph")]
+
+    infected_nodes_info=dict['infected_nodes_info']
+    print("Updating graph")
+
     G = dict['G']
     rate = dict['rate']
-    if n_clicks is not None:
-        if(n_clicks > dict['clicks']):
-            print("Creating graph")
-            dict['infected_nodes_info']={}
-            infected_nodes_info = {}
-            infected = 0
-            cont = 0
-            while infected < nodes_infected and cont<num_nodes:
-                print("Infecting...")
-                randy = random.randint(0, num_nodes+1)
-                if randy not in infected_nodes_info:
-                    infected_nodes_info[randy] = True
-                    infected += 1
-                cont += 1
 
-            print("Creating graph with %d nodes and %d as radius. Infecting %d nodes." % (num_nodes, radius, nodes_infected))
-            set_my_graph(nx.random_geometric_graph(num_nodes, radius))
-            pos = nx.get_node_attributes(G, 'pos')
-            dmin = 1
-            ncenter = 0
-            for n in pos:
-                x, y = pos[n]
-                d = (x - 0.5) ** 2 + (y - 0.5) ** 2
-                if d < dmin:
-                    ncenter = n
-                    dmin = d
-            edge_trace = go.Scatter(x=[], y=[], line={'width': 0.5, 'color': '#888'}, hoverinfo='none', mode='lines')
-            nodes = G.nodes()
-            for edge in G.edges():
-                x0, y0 = nodes[edge[0]]['pos']
-                x1, y1 = nodes[edge[1]]['pos']
-                edge_trace['x'] += tuple([x0, x1, None])
-                edge_trace['y'] += tuple([y0, y1, None])
-            node_trace = go.Scatter(x=[], y=[], text=[], mode='markers', hoverinfo='text',
-                                    marker={'showscale': True, 'colorscale': 'Picnic', 'reversescale': True, 'color': [],
-                                            'size': 10,
-                                            'colorbar': {'thickness': 10, 'xanchor': 'left',
-                                                         'titleside': 'right'},
-                                            'line': {'width': 2}})
-            nodes = G.nodes()
-            for i in range(len(nodes)):
-                x, y = nodes[i]['pos']
-                node_trace['x'] += tuple([x])
-                node_trace['y'] += tuple([y])
-            p = nx.single_source_shortest_path_length(G, ncenter)
-            for node, adjacencies in enumerate(G.adjacency()):
-                color_node = 2
-                if node in infected_nodes_info:
-                    color_node = 0
-                node_trace['marker']['color'] += tuple([color_node])
-                node_info = 'Is infected: ' + str(node in infected_nodes_info)
-                node_trace['text'] += tuple([node_info])
-            figure = {"data": [edge_trace, node_trace],
-                      "layout": go.Layout(showlegend=False, hovermode='closest',
-                                          margin={'b': 20, 'l': 5, 'r': 5, 't': 40},
-                                          xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                                          yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False})}
-            dict['G'] = G
-            print("Graph created")
-            return figure
+    # Update infected nodes in graph
+    update_infected()
 
-        # Updating graph
-        print("Updating graph...")
-        infected_nodes_info = dict['infected_nodes_info']
-        temp_infected_nodes_info = {}
-        for node in G.nodes():
-            if node in infected_nodes_info:
-                for neighbor in G.neighbors(node):
-                    if neighbor not in infected_nodes_info:
-                        posibility = [True] * int(rate * 10) + [False] * int((1-rate)*10)
-                        if random.choice(posibility):
-                            temp_infected_nodes_info[neighbor] = True
+    # Print information
+    results['s_results']=[html.P('Interval:'),
+    html.P(str(interval_n)),
+    html.P('Total nodes:'),
+    html.P(str(len(G.nodes()))),
+    html.P('Number of infected nodes'),
+    html.P(str(len(dict['infected_nodes_info'])))
+    ]
 
-        dict['infected_nodes_info'] = temp_infected_nodes_info
-        pos = nx.get_node_attributes(G, 'pos')
-        dmin = 1
-        ncenter = 0
-        for n in pos:
-            x, y = pos[n]
-            d = (x - 0.5) ** 2 + (y - 0.5) ** 2
-            if d < dmin:
-                ncenter = n
-                dmin = d
-        edge_trace = go.Scatter(x=[], y=[], line={'width': 0.5, 'color': '#888'}, hoverinfo='none', mode='lines')
-        nodes = G.nodes()
-        for edge in G.edges():
-            x0, y0 = nodes[edge[0]]['pos']
-            x1, y1 = nodes[edge[1]]['pos']
-            edge_trace['x'] += tuple([x0, x1, None])
-            edge_trace['y'] += tuple([y0, y1, None])
-        node_trace = go.Scatter(x=[], y=[], text=[], mode='markers', hoverinfo='text',
-                                marker={'showscale': True, 'colorscale': 'Picnic', 'reversescale': True, 'color': [],
-                                        'size': 10,
-                                        'colorbar': {'thickness': 10, 'xanchor': 'left',
-                                                     'titleside': 'right'},
-                                        'line': {'width': 2}})
-        nodes = G.nodes()
-        for i in range(len(nodes)):
-            x, y = nodes[i]['pos']
-            node_trace['x'] += tuple([x])
-            node_trace['y'] += tuple([y])
-        p = nx.single_source_shortest_path_length(G, ncenter)
-        for node, adjacencies in enumerate(G.adjacency()):
-            color_node = 2
-            if node in infected_nodes_info:
-                color_node = 0
-            node_trace['marker']['color'] += tuple([color_node])
-            node_info = 'Is infected: ' + str(node in infected_nodes_info)
-            node_trace['text'] += tuple([node_info])
-        figure = {"data": [edge_trace, node_trace],
-                  "layout": go.Layout(showlegend=False, hovermode='closest',
-                                      margin={'b': 20, 'l': 5, 'r': 5, 't': 40},
-                                      xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                                      yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False})}
-        dict['G'] = G
-        return figure
+    print("Interval: ")
+    print(interval_n)
+    print("Vector de estado de infección:")
+    print(infected_nodes_info)
+    print("Número de nodos infectados:")
+    print(len(dict['infected_nodes_info']))
+    print("Número de nodos totales:")
+    print(len(G.nodes()))
+
+    dict['G'] = G
+    print("Trying to draw")
+    figure = draw_a_graph()
+    print("Updated graph")
+    return figure, results['s_results']
 
 if __name__ == '__main__':
     app.run_server(debug=True)
